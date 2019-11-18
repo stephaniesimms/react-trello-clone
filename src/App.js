5052167a1061aec728b656233c9221397addbc3c
Stephanie Simms <stephanie.simms@gmail.com>
2019-11-18 15:50:41 -0800

    added ability to move tasks between columns

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { DragDropContext } from 'react-beautiful-dnd';
+import styled from 'styled-components';
 import initialData from './initialData';
 import Column from './Column';
 
+
+const Container = styled.div`
+  display: flex;
+`;
 class App extends Component {
   state = initialData;
 
@@ -23,43 +28,83 @@ class App extends Component {
     }
 
     // otherwise, update state to reflect drag/drop action
-    // const start = this.state.columns[source.droppableId];
-    // const finish = this.state.columns[destination.droppableId];
-    const column = this.state.columns[source.droppableId];
-    const newTaskIds = Array.from(column.taskIds);
+    const start = this.state.columns[source.droppableId];
+    const finish = this.state.columns[destination.droppableId];
+    
+    if (start === finish) {
+      this.moveTaskToSameList(start, source, destination, draggableId);
+    } else {
+      this.moveTaskToDifferentList(start, finish, source, destination, draggableId)
+    }
+  }
+
+  moveTaskToSameList(start, source, destination, draggableId) {
+    const newTaskIds = start.taskIds;
+    
+    // rearrange taskIds in list
     newTaskIds.splice(source.index, 1);
     newTaskIds.splice(destination.index, 0, draggableId);
 
     const newColumn = {
-      ...column,
+      ...start,
       taskIds: newTaskIds,
-    };
+    }
 
     const newState = {
       ...this.state,
       columns: {
         ...this.state.columns,
-        [newColumn.id]: newColumn,
+        [newColumn.id]: newColumn
       },
     };
 
     this.setState(newState);
+  }
   
+  moveTaskToDifferentList(start, finish, source, destination, draggableId) {
+    // remove task from start tasks
+    const newStartTaskIds = start.taskIds;
+    newStartTaskIds.splice(source.index, 1);
+    const newStartColumn = {
+      ...start,
+      taskIds: newStartTaskIds
+    }
+
+    // add task to finish tasks
+    const newFinishTaskIds = finish.taskIds;
+    newFinishTaskIds.splice(destination.index, 0, draggableId);
+    const newFinishColumn = {
+      ...finish,
+      taskIds: newFinishTaskIds
+    }
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newStartColumn.id]: newStartColumn,
+        [newFinishColumn.id]: newFinishColumn
+      },
+    };
+
+    this.setState(newState);
   }
 
   render() {
     return (
       <DragDropContext onDragEnd={this.onDragEnd}>
-        {this.state.columnOrder.map(columnId => {
-          const column = this.state.columns[columnId];
-          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+        <Container>
+          {this.state.columnOrder.map(columnId => {
+            const column = this.state.columns[columnId];
+            const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
 
-          return <Column
-            key={column.id}
-            column={column}
-            tasks={tasks}
-          />;
-        })};
+            return <Column
+              key={column.id}
+              column={column}
+              tasks={tasks}
+            />;
+          })};
+        </Container>
       </DragDropContext>
     )
   }
--- src/Column.js
+++ src/Column.js
@@ -7,6 +7,10 @@ const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgray;
   border-radius: 2px;
+  width: 220px;
+
+  display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.h3`
@@ -16,11 +20,14 @@ const Title = styled.h3`
 // update bg-color based on drag state, using snapshot
 const TaskList = styled.div`
   padding: 8px;
+  transition: background-color 0.2s ease;
   background-color: ${props => (props.isDraggingOver ? 'lightgray' : 'white')}
+  flex-grow: 1;
+  min-height: 100px;
 `;
 
 class Column extends Component {
-  
+
   render() {
     const { title, id } = this.props.column;
 
